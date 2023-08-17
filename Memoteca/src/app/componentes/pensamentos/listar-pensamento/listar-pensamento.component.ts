5b9c83fff981cdd29a0511bfe18e20c6f34c9c26
MarlonAugusto <l.marlonaugusto@gmail.com>
2023-08-17 12:40:56 -0300

    [FEAT] Finished CRUD

--- Memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ Memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento.js';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css'],
+  styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos: Pensamento[] = [];
-  constructor() {}
 
-  ngOnInit(): void {}
+  listaPensamentos: Pensamento[] = [];
+
+  constructor(private service: PensamentoService) { }
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
 }
